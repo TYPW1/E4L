a598dfe17cc2649618d7c83eb549b29a01c5bfb5
typw01 <pierretypw@gmail.com>
2023-10-31 13:07:03 +0100

    Initial commit

--- git_push_script.sh
+++ git_push_script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Initialize a new Git repository
-git init
+# Check if a Git repository needs to be initialized
+if [ ! -d ".git" ]; then
+    git init
+fi
 
-# Add the remote GitLab repository
-git remote add origin http://192.168.56.10/gitlab/typw01/e4l.git
+# Add the remote GitLab repository with a different name
+git remote add gitlab http://192.168.56.10/gitlab/typw01/e4l.git
 
 # Add all files to the staging area
 git add .
@@ -12,5 +14,8 @@ git add .
 # Commit the changes with a message
 git commit -m "Initial commit"
 
-# Push the commit to the GitLab repository on the master branch
-git push -u origin master
+# Determine the default branch name (master or main or any other)
+DEFAULT_BRANCH=$(git branch --show-current)
+
+# Push the commit to the GitLab repository on the default branch using the new remote name
+git push -u gitlab $DEFAULT_BRANCH
