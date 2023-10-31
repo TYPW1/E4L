17f666432bff3b1a02231368253a81183acdda04
typw01 <pierretypw@gmail.com>
2023-10-31 13:08:38 +0100

    Initial commit

--- git_push_script.sh
+++ git_push_script.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+# Your GitLab credentials
+GITLAB_USERNAME="typw01"
+GITLAB_PASSWORD="Pierr01999."
+
+# The URL of your GitLab repository (without credentials)
+GITLAB_REPO_URL="http://192.168.56.10/gitlab/typw01/e4l.git"
+
+# Encoded URL to include credentials
+ENCODED_GITLAB_URL=$(echo $GITLAB_REPO_URL | sed "s://:://${GITLAB_USERNAME}:${GITLAB_PASSWORD}@:")
+
 # Check if a Git repository needs to be initialized
 if [ ! -d ".git" ]; then
     git init
 fi
 
 # Add the remote GitLab repository with a different name
-git remote add gitlab http://192.168.56.10/gitlab/typw01/e4l.git
+git remote add gitlab $ENCODED_GITLAB_URL
 
 # Add all files to the staging area
 git add .
